0df19cc806ec4667a794e9924cdd4018ac092b57
Enes <66560757+devenes@users.noreply.github.com>
2022-04-17 01:34:28 +0300

    Update variables.tf

--- tf-files/variables.tf
+++ tf-files/variables.tf
@@ -37,6 +37,10 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
+variable "instance_name" {
+  default = "Web Server of Phonebook App"
+}
+
 variable "aws_launch_template_name" {
   default = "phonebook-lt"
 }
