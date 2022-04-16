c1afd7c9895920fbd4c9caf44f23affea478be26
Enes <66560757+devenes@users.noreply.github.com>
2022-04-17 01:19:26 +0300

    Create sec-gr.tf

---
+++ tf-files/sec-gr.tf
@@ -0,0 +1,73 @@
+resource "aws_security_group" "alb-sg" {
+  name   = "ALBSecurityGroup"
+  vpc_id = data.aws_vpc.default_vpc.id
+  tags = {
+    Name = "TF_ALBSecurityGroup"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "server-sg" {
+  name   = "WebServerSecurityGroup"
+  vpc_id = data.aws_vpc.default_vpc.id
+  tags = {
+    Name = "TF_WebServerSecurityGroup"
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb-sg.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_security_group" "db-sg" {
+  name   = "RDSSecurityGroup"
+  vpc_id = data.aws_vpc.default_vpc.id
+  tags = {
+    "Name" = "TF_RDSSecurityGroup"
+  }
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.server-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
