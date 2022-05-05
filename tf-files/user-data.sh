7f742872066fa8d90ed9df54ffc573b53b52f396
Enes <66560757+devenes@users.noreply.github.com>
2022-05-05 04:25:13 +0300

    Update user-data.sh

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -7,5 +7,5 @@ pip3 install flask
 pip3 install flask_mysql
 yum install git -y
 TOKEN="*************************"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/devenes/terraform-phonebook.git
-python3 /home/ec2-user/terraform-phonebook/phonebook-app.py
+cd /home/ec2-user && git clone https://$TOKEN@github.com/devenes/terraform-alb-asg-app.git
+python3 /home/ec2-user/terraform-alb-asg-app/phonebook-app.py
